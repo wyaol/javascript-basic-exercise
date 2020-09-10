cea688405fada7fe645f763cde3586af974bd9f3
wyao <289672494@qq.com>
2020-09-10 15:27:09 +0800

    exercise 5 finish

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,17 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array == null) throw new Error('Flatten undefined or null array');
+  else if (!Array.isArray(array)) throw new Error();
+  else {
+    const res = [];
+    array.forEach((item) => {
+      if (Array.isArray(item)) {
+        item.forEach(item2 => res.push(item2));
+      } else {
+        res.push(item);
+      }
+    });
+    return res;
+  }
 }
