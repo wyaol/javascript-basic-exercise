f5b8a8cf97c6b86693e381320996a1d4d45d8374
wyao <289672494@qq.com>
2020-09-10 14:36:47 +0800

    exercise 2

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,15 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  let res;
+  if (prediction instanceof Function) {
+    res = 0;
+    for (let i = 0; i < string.length; i += 1) {
+      res += prediction(string[i]) ? 1 : 0;
+    }
+  } else {
+    res = (typeof string === 'string') ? string.length : 0;
+  }
+
+  return res;
 }
