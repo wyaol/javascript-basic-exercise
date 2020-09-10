d54a12ca312fa03918bea58745c4d3d2929d0386
wyao <289672494@qq.com>
2020-09-10 15:02:03 +0800

    exercise 4 finish

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,17 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array == null) throw new Error('Creating list from undefined array');
+  else if (array.length === 0) throw new Error('Creating list from empty array');
+  else if (!Array.isArray(array)) throw new Error();
+  else {
+    const header = { value: array[0], next: null };
+    let cur = header;
+    array.forEach((element) => {
+      const newNode = { value: element, next: null };
+      cur.next = newNode;
+      cur = cur.next;
+    });
+    return header.next;
+  }
 }
