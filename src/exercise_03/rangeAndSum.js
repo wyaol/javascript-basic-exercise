0c7e754882c80a97946ef0df0de8d7b1a3dfbae2
wyao <289672494@qq.com>
2020-09-10 14:47:04 +0800

    exercise 3 finish

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,17 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const res = [];
+  if (start < end) {
+    for (let i = start; i < end; i += 1) {
+      res.push(i);
+    }
+  } else {
+    for (let i = start; i > end; i -= 1) {
+      res.push(i);
+    }
+  }
+  return res;
 }
 
 export function sum(...numbers) {
@@ -17,6 +26,7 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let res = 0;
+  numbers.forEach((item) => { res += item; });
+  return res;
 }
